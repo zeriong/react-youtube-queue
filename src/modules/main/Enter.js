312ae5bfdcc2504e15fa433e7f126b1d8776433e
jeryong <jaeryong95@gmail.com>
2024-04-06 15:26:32 +0900

    build: tailwindcss, 토스트 알림 추가를 위한 zustand 추가

--- src/modules/main/Enter.js
+++ src/modules/main/Enter.js
@@ -1,6 +1,21 @@
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+
 const Enter = () => {
+    const [nickName, setNickName] = useState();
+    const [certificate, setCertificate] = useState();
+
+    const navigate = useNavigate();
+
+    const submitCertificateNumber = () => {
+        if (certificate === process.env.CERTIFICATE_NUMBER) navigate("/queue");
+    }
+
+    useEffect(() => {
+
+    }, []);
     return (
-        <div>
+        <div className="">
             메인이고 엔터임
         </div>
     )
