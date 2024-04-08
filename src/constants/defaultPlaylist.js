fbadbaecc72cc3be63a054fa590006757ab48a21
jeryong <jaeryong95@gmail.com>
2024-04-08 23:42:45 +0900

    feat: 기본 Lofi음악 리스트 생성 후 랜덤 재생(중복 없이 셔플) 가능하도록 설계, 주석으로 설계 방향성 기록

---
+++ src/constants/defaultPlaylist.js
@@ -0,0 +1,15 @@
+export const DEFAULT_PLAYLIST = [
+    "https://youtu.be/p6xqKJqsQWs?si=pmhPrEr2-MF0IofE",
+    "https://youtu.be/oaIJNOqY1oQ?si=bfzjRfR751unUU4r",
+    "https://youtu.be/nhAR2-WIM-I?si=G5UtEE9Ku1fuoQr7",
+    "https://youtu.be/IHLLYeFc5sg?si=76HEFFNppRPIIFgo",
+    "https://youtu.be/rJVYb2Ib8DU?si=Si9I2XcfXENpN2Jf",
+    "https://youtu.be/Xc1Le3CSdrM?si=XFF1bkMhJyW6uJ73",
+    "https://youtu.be/oaIJNOqY1oQ?si=Y5IlwlLIRRj5t76j",
+    "https://youtu.be/-3CDcU-eco8?si=QLWIG1KEtX8x-d8p",
+    "https://youtu.be/9pdi1twTpAs?si=Y8SFHDu968HK15yY",
+    "https://youtu.be/IHLLYeFc5sg?si=nOv29DWKhj4DaYHd",
+    "https://youtu.be/2mRKDQ0h1ao?si=vv4nj0MTSyk9g08U",
+    "https://youtu.be/Im2Ord5fMu0?si=pVPIp-2ygmunYTL-",
+    "https://youtu.be/DrKcJXKiVnA?si=1JWrfZSnDSgKhcnF",
+];
--- src/modules/main/YoutubeQueuePlay.jsx
+++ src/modules/main/YoutubeQueuePlay.jsx
@@ -4,12 +4,13 @@ import {useToastsStore} from "../common/components/Toasts";
 import {useNavigate} from "react-router-dom";
 import ReactPlayer from "react-player";
 import {useEffect, useRef, useState} from "react";
+import {DEFAULT_PLAYLIST} from "../../constants/defaultPlaylist";
 
 const YoutubeQueuePlay = () => {
     const playerRef = useRef(null);
+    const shuffleRef = useRef([]);
 
     const [currentURL, setCurrentURL] = useState("https://www.youtube.com/watch?v=_GNk6lSvH08");
-    const [currentProgress, setCurrentProgress] = useState(0);
     const [submitList, setSubmitList] = useState([]);
     const [submitInput, setSubmitInput] = useState("");
 
@@ -23,8 +24,7 @@ const YoutubeQueuePlay = () => {
         navigate("/")
         toastStore.addToast("로그아웃 되었습니다.");
     }
-    // 현재 재생시간
-    const getCurrentProgress = (e) => setCurrentProgress(e.playedSeconds);
+
     // 신청곡 url submit
     const submitURL = (e) => {
         e.preventDefault();
@@ -32,20 +32,51 @@ const YoutubeQueuePlay = () => {
         setSubmitList(prev => [...prev, list]);
     }
 
+    // 기본 Lofi음악 리스트 랜덤 재생
+    const defaultPlayer = () => {
+        // 난수 생성
+        const randomNum = Math.floor(Math.random() * DEFAULT_PLAYLIST.length);
+        // 난수가 이미 배열에 존재하면 재귀하여 재생성
+        if (shuffleRef.current.some(val => randomNum === val)) return defaultPlayer();
+        // 존재하지 않는 난수를 생성 시 셔플ref에 푸시
+        shuffleRef.current.push(randomNum);
+        // 해당 번호의 리스트를 currentURL에 setState
+        setCurrentURL(DEFAULT_PLAYLIST[randomNum]);
+        // 셔플ref가 가득 차면 초기화
+        if (shuffleRef.current.length === DEFAULT_PLAYLIST.length) shuffleRef.current = [];
+    }
+
+    // currentURL effect
     useEffect(() => {
-    }, [currentProgress]);
+        // currentURL이 변경되면 변경된 url 로드 후 즉시 재생되도록 구현
+    }, [currentURL]);
+
+    // init effect
+    useEffect(() => {
+        // todo: 아래 설계를 함수로 만든 후 실행되도록 설계 (onEnded 에 사용할 가능성 매우 높음)
+        // 파이어베이스 데이터 가져와서 .shift() 사용해서 데이터를 셀렉트/잘라내기 하여
+        // setCurrentURL(shiftObj.url)을 넣은 후 파이어베이스에서 해당 객체 삭제
+        // 이 후에 currentURL state 변경으로 인해 바로 위 useEffect 가 감지하여 실행하도록 설계
+
+        // 만약 받아온 데이터가 없다면 여기에서 즉시 랜덤플레이어 실행되도록 설정
+    }, []);
 
     return (
         <>
             <div className="flex w-full h-full">
                 {/* 플레이어 래퍼 */}
                 <section className="w-[500px] h-[300px]">
+                    {/*
+                    어드민은 플레이어 보여주고
+                    아닌 인증자는 아래 문구와 함께 검정 박스 ㄱㄱ
+                    "음악 재생은 어드민 인증자에게 맡겨주세요!"
+                    "신청곡은 신청하고 삭제도 가능합니다."
+                    */}
                     <ReactPlayer
                         url={currentURL}
                         width='100%'
                         height='100%'
                         controls={true}
-                        onProgress={getCurrentProgress}
                         onEnded={() => {
                             // 동영상이 끝나면 실행할 함수
                             console.log("동영상 끝남")
