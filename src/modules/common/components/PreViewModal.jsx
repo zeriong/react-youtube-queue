c7d4b4f5d523c4478ec8a4f85118a03725a084a7
jeryong <jaeryong95@gmail.com>
2024-04-10 20:48:57 +0900

    feat: 미리보기 모달 추가

--- src/modules/common/components/PreViewModal.jsx
+++ src/modules/common/components/PreViewModal.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import {useEffect} from "react";
+import ReactPlayer from "react-player";
+import {CloseIcon} from "../../svgComponents";
 
-const PreViewModal = ({ isShow, setIsShow, preViewURL }) => {
-
-    useEffect(() => {
-
-    }, [isShow]);
-
+const PreViewModal = ({ isShow, setIsShow, preViewData }) => {
     return isShow &&
-        <>
-            {/* 모달 배경 영역 */}
-            <div onClick={() => setIsShow(false)} className="fixed top-0 left-0 z-50 w-full h-full bg-black/50"/>
+        <div onClick={() => setIsShow(false)} className="fixed top-0 left-0 z-50 w-full h-full bg-black/50 flex justify-center items-center">
+            {/* 미리보기 영역 */}
+            <section className="p-3 max-w-[500px] max-h-[500px] w-full h-full">
 
-            <div className="absolute left-1/2 top-1/2 z-[100] w-[300px] h-[300px] bg-white">
-                호잇:
-                {preViewURL}
-            </div>
-        </>
+                <div
+                    onClick={(e) => e.stopPropagation()}
+                    className="bg-white w-full h-full flex flex-col rounded-2xl"
+                >
+                    {/* 모달 헤더 */}
+                    <header className="py-2 px-2 flex justify-between">
+                        <p>{ `${preViewData.nickName}님의 신청곡` }</p>
+                        <button type="button" onClick={() => setIsShow(false)}>
+                            <CloseIcon/>
+                        </button>
+                    </header>
+                    {/* 플레이어 영역 */}
+                    <section className="grow px-2 pb-2">
+                        <div className="rounded-xl overflow-hidden h-full w-full">
+                            <ReactPlayer
+                                width="100%"
+                                height="100%"
+                                controls={true}
+                                url={preViewData.link}
+                            />
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </div>
 }
 
 export default PreViewModal;
