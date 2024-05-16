a8f04d0e5ebf9dc327a9145145c584166b9b9a0d
jeryong <jaeryong95@gmail.com>
2024-05-16 18:55:40 +0900

    fate: 사다리타기 게임 컴포넌트 추가(기능 미완), 라우터에서 lazy import 가능하도록 Suspense 추가 및 not found 추가

---
+++ src/modules/main/games/ghostLeg/GhostLeg.jsx
@@ -0,0 +1,9 @@
+const GhostLeg = () => {
+    return (
+        <>
+            <div>사다리타기 게임~~ ^^</div>
+        </>
+    )
+}
+
+export default GhostLeg;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,19 +1,36 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Enter from "../modules/intro/Enter";
 import PrivateComponent from "./components/PrivateComponent";
-import YoutubeQueuePlay from "../modules/main/YoutubeQueuePlay";
+import YoutubeQueuePlay from "../modules/main/youtubePlayer/YoutubeQueuePlay";
+import {lazy, Suspense} from "react";
+import NotFound from "../modules/common/notFound/NotFound";
+
+const GhostLeg = lazy(import("../modules/main/games/ghostLeg/GhostLeg"))
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route element={<PrivateComponent isOnlyNonCertificate={true}/>}>
-                    <Route path="/" element={<Enter/>}/>
-                </Route>
-                <Route element={<PrivateComponent/>}>
-                    <Route path="/queuePlayer" element={<YoutubeQueuePlay/>}/>
-                </Route>
-            </Routes>
+            <Suspense>
+                <Routes>
+                    <Route element={<PrivateComponent isOnlyNonCertificate={true}/>}>
+                        <Route path="/" element={<Enter/>}/>
+                    </Route>
+
+                    {/* contents */}
+                    <Route element={<PrivateComponent/>}>
+                        <Route path="/queuePlayer" element={<YoutubeQueuePlay/>}/>
+                    </Route>
+
+                    {/* games */}
+                    <Route path="games">
+                        <Route index element={<NotFound/>} />
+                        <Route path="ghostLeg" element={<GhostLeg/>}/>
+                    </Route>
+
+                    {/* NotFound */}
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
