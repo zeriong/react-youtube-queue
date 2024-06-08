10e20e355fe92eac69cd52273c9fb1eca6556ed4
jeryong <jaeryong95@gmail.com>
2024-06-08 23:29:35 +0900

    refactor: contentList: desc 추가

--- src/constants/contentList.js
+++ src/constants/contentList.js
@@ -2,17 +2,21 @@ export const CONTENT_LIST = [
     {
         path: "player",
         title: "유튜브 플레이어",
+        desc: "원하는 유튜브 음악의 URL을 신청하고 같은 공간에서 함께 음악감상을 할 수 있는 컨텐츠에요.",
     },
     {
         path: "ghostLeg",
         title: "사타리 타기",
+        desc: "준비중인 컨텐츠입니다.",
     },
     {
         path: "poll",
         title: "투표",
+        desc: "준비중인 컨텐츠입니다.",
     },
     {
         path: "tetris",
         title: "테트리스",
+        desc: "준비중인 컨텐츠입니다.",
     },
 ];
