06f30c2db13c087daf94ed44a26e9dffe4842e04
jeryong <jaeryong95@gmail.com>
2024-06-09 23:48:58 +0900

    chore: 대시보드 준비중인 컨텐츠(Prepare 컴포넌트) 삭제

--- src/modules/main/dashBoard/DashBoard.jsx
+++ src/modules/main/dashBoard/DashBoard.jsx
@@ -6,28 +6,28 @@ import {Link} from "react-router-dom";
 const DashBoard = () => {
     return (
         <div className="w-full min-w-full h-full flex justify-center">
-            {(isDev) ? (
-                <div className="max-w-[1300px] w-full py-[40px]">
-                    <p className="text-[40px] font-bold mb-[12px]">컨텐츠</p>
-                    <ul className="grid grid-cols-4 flex-wrap gap-4 py-4">
-                        {CONTENT_LIST.map((item, idx) => {
-                            return (
-                                <Link
-                                    key={idx}
-                                    to={item.path}
-                                    className="border-2 relative transition-all ease-in-out border-gray-300 p-4 overflow-hidden rounded-md shadow-lg
+            <div className="max-w-[1300px] w-full py-[40px]">
+                <p className="text-[40px] font-bold mb-[12px]">컨텐츠</p>
+                <ul className="grid grid-cols-4 flex-wrap gap-4 py-4">
+                    {CONTENT_LIST.map((item, idx) => {
+                        return (
+                            <Link
+                                key={idx}
+                                to={item.path}
+                                className="border-2 relative transition-all ease-in-out border-gray-300 p-4 overflow-hidden rounded-md shadow-lg
                                      bottom-0 hover:bottom-2"
-                                >
-                                    <p className="font-bold mb-2 text-[20px]">{item.title}</p>
-                                    <p className="whitespace-break-spaces text-[15px] text-gray-800">{item.desc}</p>
-                                </Link>
-                            )
-                        })}
-                    </ul>
-                </div>
-            ) : (
-                <Prepare/>
-            )}
+                            >
+                                <p className="font-bold mb-2 text-[20px]">
+                                    {item.title}
+                                </p>
+                                <p className="whitespace-break-spaces text-[15px] text-gray-800">
+                                    {item.desc}
+                                </p>
+                            </Link>
+                        )
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
