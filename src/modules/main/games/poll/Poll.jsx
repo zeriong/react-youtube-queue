59570e84128da4efc9a0e2a2eb2d50ef1ddc61f6
jeryong <jaeryong95@gmail.com>
2024-06-10 23:56:28 +0900

    feat: 투표 및 앞으로 개발 방향성에 대한 설계 기록 및 준비

--- src/modules/main/games/poll/Poll.jsx
+++ src/modules/main/games/poll/Poll.jsx
@@ -1,16 +1,46 @@
 import {isDev} from "../../../../App";
 import Prepare from "../../../common/Prepare";
+import {useEffect, useState} from "react";
 
 const Poll = () => {
+    const [pollData, setPollData] = useState();
+
+    // 투표 데이터 get fetcher
+    const getPollData = () => {
+        (async () => {
+            console.log("가져왔다 투표데이터~");
+        })()
+    }
+
+    useEffect(() => {
+        getPollData();
+    }, []);
     return (
         <div className="min-w-full">
-            <>
-                {(isDev) ? (
-                    <div className="">투표!!!</div>
-                ) : (
-                    <Prepare/>
-                )}
-            </>
+            {(isDev) ? (
+                <div className="">
+                    <button type="button">
+                        누르면 모달 띄워지고 투표 생성 가능.<br/>
+                        투표들은 마음껏 할 수 있지만 투표 기간은 반드시 정해야 함.<br/>
+                        데이트픽커 사용할거고 최대 지정일은 1주일로 할 것임.<br/>
+                        그리고 투표는 최대 5가지 선택 사항을 고를 수 있음<br/>
+                        누가 투표를 시작했는지 알 수 있고 투표 제목도 보여져야 함<br/>
+                        슬슬 회원제를 도입해야 할 것 같음... 이건 약용 가능성이 너무 커서<br/>
+                        서버도 슬슬 만들기 시작해야할 것 같음...<br/>
+                        점점 스케일이 커지네...<br/>
+                        귀찮기도 함.<br/>
+                        그냥 OAuth 구현하고... 그걸로 유저 하자.<br/>
+                        추가적으로 닉네임은 변경 가능하도록 하고 닉네임 변경 시 중복검사.<br/>
+                        파이어베이스 별도로 사용 가능하도록 설정도 고려해보기<br/>
+                    </button>
+                    <ul>
+                        투표 리스트 뿌려줄 것임.
+                        박스 형식 그리드로 정렬
+                    </ul>
+                </div>
+            ) : (
+                <Prepare/>
+            )}
         </div>
     )
 }
