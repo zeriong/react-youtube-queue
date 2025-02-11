90e9cc5f946fd94a91c4a4ec07ecf04ee9be355d
jaery <jaeryong95@gmail.com>
2025-02-11 19:35:53 +0900

    chore: 재생이 불가능한 플레이리스트 삭제

--- src/constants/defaultPlaylist.js
+++ src/constants/defaultPlaylist.js
@@ -1,13 +1,7 @@
 export const DEFAULT_PLAYLIST = [
     "https://www.youtube.com/live/E1odJ0DAYMo?si=MKuhs09_FSGoj8Fj",
-    "https://www.youtube.com/live/NbH7xV7bsbU?si=wUuJLnkMS9SPo5Xw",
     "https://www.youtube.com/live/DkMz7Kbi4MQ?si=M8H9bpIKUcZv-Fai",
-    "https://www.youtube.com/live/oW2yNZ_fUUQ?si=62UhDiX-kR-DyTDM",
     "https://www.youtube.com/live/jxz9SaqvTKY?si=U7VcVYkktNvbLJwk",
     "https://www.youtube.com/live/dRDbmBVJ43w?si=HImrhsUulJlm3tmi",
-    "https://www.youtube.com/live/yFQ6WMhlnto?si=LHAeIf8b5Kb4YLgI",
-    "https://www.youtube.com/live/Vw1TWfBI4gs?si=H5-ButzIXqBNLu5A",
     "https://www.youtube.com/live/6KbLIieGmZ8?si=Dns6heC0nDrTn6cM",
-    "https://youtu.be/ZI9jHPA6g_I?si=GSlFjipWJlNnhgr8",
-    "https://www.youtube.com/live/jO1v9ORwDz0?si=-LGGEjb0Gl9O-i_x",
 ];
