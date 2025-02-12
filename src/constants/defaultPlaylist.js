05e9f0398bd3027dc5a0eb168ee844c512672426
jaery <jaeryong95@gmail.com>
2025-02-12 19:45:43 +0900

    chore: 기본 플레이 리스트 일부 변경

--- src/constants/defaultPlaylist.js
+++ src/constants/defaultPlaylist.js
@@ -4,4 +4,9 @@ export const DEFAULT_PLAYLIST = [
     "https://www.youtube.com/live/jxz9SaqvTKY?si=U7VcVYkktNvbLJwk",
     "https://www.youtube.com/live/dRDbmBVJ43w?si=HImrhsUulJlm3tmi",
     "https://www.youtube.com/live/6KbLIieGmZ8?si=Dns6heC0nDrTn6cM",
+    "https://youtu.be/uq27xkfkTp0?si=FpPq2vZRWb_olaHw",
+    "https://youtu.be/cbuZfY2S2UQ?si=aMXGk3HN8gHMAvCw",
+    "https://youtu.be/TURbeWK2wwg?si=uPX0eaAJlihJZctS",
+    "https://youtu.be/lA9FONoiuFA?si=J9aBP--t_tgk_kvL",
+    "https://youtu.be/WBnjO3zEwCM?si=qX_TU464hhnrVTNL",
 ];
