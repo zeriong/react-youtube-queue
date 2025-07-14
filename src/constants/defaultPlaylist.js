f8a7e8827024a6af8d72a0522cb8b3af7e267b31
zeriong <jaeryong95@gmail.com>
2025-07-14 09:08:36 +0900

    refactor: 실행되지 않는 default영상 삭제 후 새로운 lofi 음악 추가

--- src/constants/defaultPlaylist.js
+++ src/constants/defaultPlaylist.js
@@ -1,12 +1,13 @@
 export const DEFAULT_PLAYLIST = [
     "https://www.youtube.com/live/E1odJ0DAYMo?si=MKuhs09_FSGoj8Fj",
-    "https://www.youtube.com/live/DkMz7Kbi4MQ?si=M8H9bpIKUcZv-Fai",
-    "https://www.youtube.com/live/jxz9SaqvTKY?si=U7VcVYkktNvbLJwk",
+    "https://youtu.be/NtjphDEmzqE?si=bmHAoj2olSv8rhmR",
+    "https://youtu.be/NjMXr-c6LIU?si=9w9so3e_31NENA_l",
+    "https://youtu.be/OAywOZPuY0Y?si=PAd8Eo7bmJdGPtm4",
     "https://www.youtube.com/live/dRDbmBVJ43w?si=HImrhsUulJlm3tmi",
     "https://www.youtube.com/live/6KbLIieGmZ8?si=Dns6heC0nDrTn6cM",
-    "https://youtu.be/uq27xkfkTp0?si=FpPq2vZRWb_olaHw",
     "https://youtu.be/cbuZfY2S2UQ?si=aMXGk3HN8gHMAvCw",
     "https://youtu.be/TURbeWK2wwg?si=uPX0eaAJlihJZctS",
     "https://youtu.be/lA9FONoiuFA?si=J9aBP--t_tgk_kvL",
-    "https://youtu.be/WBnjO3zEwCM?si=qX_TU464hhnrVTNL",
+    "https://youtu.be/aWV6uv8Que0?si=sAQIe1wqwT1814cJ",
+    "https://youtu.be/DHI_lLBSabI?si=hP8JVzWVinvfY48H",
 ];
