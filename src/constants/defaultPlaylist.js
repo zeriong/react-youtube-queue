c093878b13287c6bfdbed3bb2daf259ab757ef2b
zeriong <jaeryong95@gmail.com>
2025-07-14 09:16:42 +0900

    refactor: 기존 main branch에 있던 기본음악 리스트 병합

--- src/constants/defaultPlaylist.js
+++ src/constants/defaultPlaylist.js
@@ -5,6 +5,9 @@ export const DEFAULT_PLAYLIST = [
     "https://youtu.be/OAywOZPuY0Y?si=PAd8Eo7bmJdGPtm4",
     "https://www.youtube.com/live/dRDbmBVJ43w?si=HImrhsUulJlm3tmi",
     "https://www.youtube.com/live/6KbLIieGmZ8?si=Dns6heC0nDrTn6cM",
+    "https://youtu.be/cbuZfY2S2UQ?si=aMXGk3HN8gHMAvCw",
+    "https://youtu.be/TURbeWK2wwg?si=uPX0eaAJlihJZctS",
+    "https://youtu.be/lA9FONoiuFA?si=J9aBP--t_tgk_kvL",
     "https://youtu.be/aWV6uv8Que0?si=sAQIe1wqwT1814cJ",
     "https://youtu.be/DHI_lLBSabI?si=hP8JVzWVinvfY48H",
 ];
