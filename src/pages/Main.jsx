a1f98624dc429b0e38c00ff0407c4777d30d88f7
zeriong <jaeryong95@gmail.com>
2025-12-02 22:16:56 +0900

    chore: FSD 아키텍처 pages 구성

---
+++ src/pages/Main.jsx
@@ -0,0 +1,47 @@
+import { useParams } from "react-router-dom";
+import DashBoard from "../modules/main/dashBoard/DashBoard";
+import YoutubeQueuePlay from "../modules/main/youtubePlayer/YoutubeQueuePlay";
+import GhostLeg from "../modules/main/games/ghostLeg/GhostLeg";
+import Poll from "../modules/main/games/poll/Poll";
+import { useEffect, useRef } from "react";
+import Tetris from "../modules/main/games/tetris/Tetris";
+import { CONTENT_LIST } from "../constants/contentList";
+
+const Main = () => {
+  const containerRef = useRef();
+  const param = useParams()["*"];
+
+  // 섹션이동 이동을 위한 param effect
+  useEffect(() => {
+    if (containerRef.current) {
+      const find = CONTENT_LIST.findIndex((item) => item.path === param);
+      if (find === undefined) containerRef.current.style.left = "0";
+      else containerRef.current.style.left = `-${(find + 1) * 100}%`;
+    }
+  }, [param]);
+
+  return (
+    <div
+      ref={containerRef}
+      style={{ transition: "ease-in-out 500ms" }}
+      className="flex absolute w-full h-full"
+    >
+      {/* Default: DashBoard */}
+      <DashBoard />
+
+      {/* contents */}
+      <YoutubeQueuePlay />
+
+      {/* games */}
+      <GhostLeg />
+
+      {/* poll */}
+      <Poll />
+
+      {/* Tetris */}
+      <Tetris />
+    </div>
+  );
+};
+
+export default Main;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,38 +1,38 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // init import
 import PrivateComponent from "./components/PrivateComponent";
 import NotFound from "../modules/common/NotFound";
 import Layout from "../modules/main/layout/Layout";
-import Enter from "../modules/intro/Enter";
+import Home from "../pages/Home";
 import Poll from "../modules/main/games/poll/Poll";
 import GhostLeg from "../modules/main/games/ghostLeg/GhostLeg";
 import YoutubeQueuePlay from "../modules/main/youtubePlayer/YoutubeQueuePlay";
 import DashBoard from "../modules/main/dashBoard/DashBoard";
-import Main from "../modules/main";
+import Main from "../pages/Main";
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* 초기 접속 페이지 */}
-                <Route element={<PrivateComponent isOnlyNonCertificate={true}/>}>
-                    <Route path="/" element={<Enter/>}/>
-                </Route>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 초기 접속 페이지 */}
+        <Route element={<PrivateComponent isOnlyNonCertificate={true} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
 
-                {/* 인증 컨텐츠 모두 비공개 */}
-                <Route path="main/*" element={<PrivateComponent/>}>
-                    {/* Layout Style */}
-                    <Route element={<Layout/>}>
-                        <Route path="*" element={<Main/>}/>
-                    </Route>
-                </Route>
+        {/* 인증 컨텐츠 모두 비공개 */}
+        <Route path="main/*" element={<PrivateComponent />}>
+          {/* Layout Style */}
+          <Route element={<Layout />}>
+            <Route path="*" element={<Main />} />
+          </Route>
+        </Route>
 
-                {/* Not Found */}
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+        {/* Not Found */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
