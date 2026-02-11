356eabca085266e5ee4f416a6052b0f383995f4e
zeriong <jaeryong95@gmail.com>
2026-02-11 20:49:52 +0900

    feat: vite-mig - YouTube Player 버튼/리스트/모달 컴포넌트 TypeScript 변환

---
+++ vite-mig/src/features/youtube-player/ui/buttons/PlayNextButton.tsx
@@ -0,0 +1,24 @@
+import { twMerge } from "tailwind-merge";
+
+interface PlayNextButtonProps {
+  onClick: () => void;
+  classNames?: string;
+}
+
+const PlayNextButton = ({ onClick, classNames }: PlayNextButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={twMerge(
+        "border-2 border-gray-700 play-next bg-black text-white",
+        "h-[80px] w-[110px] text-[18px] font-bold hover:scale-110",
+        classNames
+      )}
+      onClick={onClick}
+    >
+      <p className="relative -translate-x-[5px]">다음 곡</p>
+    </button>
+  );
+};
+
+export default PlayNextButton;
---
+++ vite-mig/src/features/youtube-player/ui/buttons/SavedMusicListButton.tsx
@@ -0,0 +1,18 @@
+import { SavedPlayList } from "@/shared/ui/icons";
+import { usePlayerStore } from "@/entities/player/model";
+
+const SavedMusicListButton = () => {
+  const { setIsShowSavedListModal } = usePlayerStore();
+
+  return (
+    <button
+      type="button"
+      className="fixed left-[20px] top-[20px] border-4 border-gray-800 rounded-lg"
+      onClick={() => setIsShowSavedListModal(true)}
+    >
+      <SavedPlayList width={50} height={50} />
+    </button>
+  );
+};
+
+export default SavedMusicListButton;
